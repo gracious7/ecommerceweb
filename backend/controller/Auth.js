5443e3fd186b0403355ed0d0c04a5a8ee453dba4
Prince <princetripathi093@gmail.com>
2023-08-21 15:42:46 +0530

    updated backend

--- backend/controller/Auth.js
+++ backend/controller/Auth.js
@@ -78,8 +78,7 @@ exports.resetPasswordRequest = async (req, res) => {
     await user.save();
 
     // Also set token in email
-    const resetPageLink =
-      'http://localhost:3000/reset-password?token=' + token + '&email=' + email;
+    const resetPageLink ='http://localhost:3000/reset-password?token=' + token + '&email=' + email;
     const subject = 'reset password for e-commerce';
     const html = `<p>Click <a href='${resetPageLink}'>here</a> to Reset Password</p>`;
 
