52948de3e086c38460b9f651da4ee5b93ecd2bff
Prince Tripathi <100070003+gracious7@users.noreply.github.com>
2023-08-23 01:18:01 +0530

    Update cartSlice.js

--- frontend/src/features/cart/cartSlice.js
+++ frontend/src/features/cart/cartSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { addToCart, deleteItemFromCart, fetchItemsByUserId, resetCart, updateCart } from './cartAPI';
+import { Toaster, toast } from 'react-hot-toast';
 
 const initialState = {
   status: 'idle',
