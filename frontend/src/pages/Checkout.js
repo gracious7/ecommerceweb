78539a3544b06d7c3f156278a0cc235a901f8763
Prince Tripathi <100070003+gracious7@users.noreply.github.com>
2023-08-23 01:47:04 +0530

    Update Checkout.js

--- frontend/src/pages/Checkout.js
+++ frontend/src/pages/Checkout.js
@@ -62,7 +62,7 @@ function Checkout() {
       const taxRate = calculateTaxRate(item);
       const priceAfterDiscount = Math.round(item.product.price * (1 - item.product.discountPercentage / 100));
       const itemTotal = priceAfterDiscount * item.quantity * (1 + taxRate);
-      return (itemTotal + amount.toFixed(2));
+      return (itemTotal + amount);
     },
     0
   );
@@ -73,7 +73,7 @@ function Checkout() {
       const taxRate = calculateTaxRate(item);
       const priceAfterDiscount = Math.round(item.product.price * (1 - item.product.discountPercentage / 100));
       const itemTotal = priceAfterDiscount * item.quantity * ( taxRate);
-      return itemTotal.toFixed(2) ;
+      return itemTotal;
     },
     0
   );
